33c967b2b3a4cf755ecec5fb885c8b1ae4e7f572
Anuj <anujshah1859@gmail.com>
2019-01-17 10:08:00 +0530

    Only addition

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+ public class Calculator
+  {
+    public static void main(String args[])
+     {
+       float a, b, res;
+       char choice, ch;
+       Scanner scan = new Scanner(System.in);
+	      
+      	do
+         {
+           System.out.print("1. Addition\n");
+           System.out.print("5. Exit\n\n");
+           System.out.print("Enter Your Choice : ");
+           choice = scan.next().charAt(0);
+           switch(choice)
+            {
+              case '1' : System.out.print("Enter Two Number : ");
+                         a = scan.nextFloat();
+                         b = scan.nextFloat();
+                         res = a + b;
+                         System.out.println("Result = " + res);
+			 break;               				  	                         									                       
+	 
+		case '5' : System.exit(0);
+			   break;
+	      default : System.out.print("Wrong Choice!!!");
+	                break;
+	   }
+																	            System.out.print("\n---------------------------------------\n");
+	}while(choice != 5);       
+    }
+}
